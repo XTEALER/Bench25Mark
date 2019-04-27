fe0991e3fb627495ddba13384685b6f0fade65ee
XTEALER <humberto0118@gmail.com>
2019-04-27 01:02:53 -0500

    python function optimized

--- Python/TimeMeasure.py
+++ Python/TimeMeasure.py
@@ -5,21 +5,21 @@ import time as t
 # COMPATIBLE WITH ALL PYTHON VERSIONS
 # use as wish :) @xtealer
 
-number = []
+Count = 25000000
+number = 0
 random.seed()
 
 # STARTS TIMER
 start = t.time()
 # ---------------------------your-code-here------------------------------
 
-Count = 25000000
 for x in range(Count):
-    number.append(random.randrange(1000000000, 9999999999))
-    print('NUMBER[%lu] = %lu' % (x + 1, number[x]))
+    number = random.randrange(1000000000, 9999999999)
+    print('NUMBER[%lu] = %lu' % (x + 1, number))
 
 # ------------------------------------------------------------------------
 stop = t.time()
 # STOPS TIMER
 
 finalTime = stop - start
-print('\n>>> %.2f SECONDS TAKEN! ' % (finalTime))
+print('\n>>> %.2f SECONDS TAKEN (PYTHON)! ' % (finalTime))
